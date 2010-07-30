4dc6411f825e14452785b023bf07ed1a05e58b31
Keith Daulton <d13@users.noreply.github.com>
2010-07-30 03:58:13 +0000

    Config updates

--- WimpyMVC-lite/private/system/library/Config.php
+++ WimpyMVC-lite/private/system/library/Config.php
@@ -3,31 +3,35 @@
 class Config {
 	private static $config;
 	public static function load(){
-		$config_fp = './config.json';
-		if (file_exists($config_fp)) {
-			$items = array();
-			self::$config = json_decode(file_get_contents($config_fp), TRUE);
-			foreach (self::$config as $key => $value) {
-				if (strpos($key, "_path") > -1) {
-					$def_value = self::$config['system.base_dir'].self::$config[$key];
-				} else {
-					$def_value = self::$config[$key];
-				}
-				if (strpos($key, "system.") > -1) {
-					$def_name = strtoupper(substr($key,7));
-					define($def_name,$def_value);
+		define("BASE_DIR",$_SERVER['DOCUMENT_ROOT']);
+		$config_list = array('/wimpy.config.json','/application.config.json');
+		foreach ($config_list as $config_file) {
+			$config_fp = BASE_DIR.'/private'.$config_file;
+			if (file_exists($config_fp)) {
+				$items = array();
+				self::$config = json_decode(file_get_contents($config_fp), TRUE);
+				foreach (self::$config as $key => $value) {
+					if (strpos($key, "_path") > -1) {
+						$def_value = BASE_DIR.self::$config[$key];
+					} else {
+						$def_value = self::$config[$key];
+					}
+					if (strpos($key, "system.") > -1) {
+						$def_name = strtoupper(substr($key,7));
+						define($def_name,$def_value);
+					}
 				}
 			}
+			else { die('Configuration missing'); }
 		}
-		else { die('Configuration missing'); }
 	}
 	public static function get($key) {
 		if(empty(self::$config)) {
 			self::load();
 		}
 		$def_value = self::$config[$key];
-		if ((strpos($key,'_path') > -1)&&(strpos($key, self::$config['system.base_dir']) === FALSE)) {
-			$def_value = self::$config['system.base_dir'].$def_value;
+		if ((strpos($key,'_path') > -1)&&(strpos($key, BASE_DIR) === FALSE)) {
+			$def_value = BASE_DIR.$def_value;
 		}
 		return $def_value;
 	}
