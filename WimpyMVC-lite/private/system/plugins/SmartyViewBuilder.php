83d6ce6e216ba37d6da98d9ddaffac2c1c570b69
Keith Daulton <d13@users.noreply.github.com>
2011-07-26 15:02:18 +0000

    Moved hard-coded configs in the Smarty class to the SmartyViewBuilder

--- WimpyMVC-lite/private/system/plugins/SmartyViewBuilder.php
+++ WimpyMVC-lite/private/system/plugins/SmartyViewBuilder.php
@@ -13,6 +13,12 @@ class SmartyViewBuilder implements ViewBuilder {
 		$smarty->debugging = false;
 		$smarty->caching = false;
 		
+		$this->template_dir = array(VIEW_PATH);
+        $this->compile_dir = SMARTY_COMPILE_PATH;
+        $this->plugins_dir = array(SMARTY_PLUGINS_DIR);
+        $this->cache_dir = SMARTY_CACHE_PATH;
+        $this->config_dir = SMARTY_VIEW_CFG_PATH;
+		
 		foreach($modelMap as $key => $value) {
 			$smarty->assign($key,$value);
 		}
